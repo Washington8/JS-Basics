791ac71f6178170545c31a3845ceeec353270491
Washington8 <60644111+Washington8@users.noreply.github.com>
2020-07-17 10:49:37 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+//performing a task
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+greet('Janet', 'Washington');
+
+//calculating a value
+function square(number) {
+    return number * number;
+}
+
+console.log(square(20));
